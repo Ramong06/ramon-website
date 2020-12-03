ee2b86979201b80b2ed8baa0763fc6edf79af10b
RG-Mac <ramong5406@gmail.com>
2020-12-03 12:05:17 -0600

    added skills bar to the side of page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -17,20 +17,14 @@ function Resume() {
         <h1 className="resumeTitle">Ramon Garza III</h1>
         <h5 className="resumeInfoHeader">(832) 773-7728 | ramong5406@gmail.com | Katy, TX 77493</h5>
         <Row>
-          <h2>PROFESSIONAL SUMMARY</h2>
-          <p>Front-End React Developer/Full Stack Developer leveraging nine years in oil and gas. Has a strong passion for innovation and a
-          keen eye for aesthetics and presentation as well as knowledge of Continuous integration/deployment. Recently obtained a certificate
-          in web development from the University of Texas, gaining experience while also honing my skills in HTML, CSS , Bootstrap, Javascript,
-          React and SQL. Excellent communication skills and collaborator who works well on team-based projects as well as independently. Recognized
-          for problem-solving and ability to adapt and implement new information as a part of a fast-paced environment.</p>
-        </Row>
-        <Row>
+          <Col size="md-4 sm-6 justify-content-md-center">
+        <div>
           <h2>TECHNICAL SKILLS</h2>
           <p>Node.Js., Express, JavaScript, ES 6+, jQuery, React.js, React 16.8+/Redux, JSX, GIT, GitHub, MongoDB, MySQL, Mongoose, Handlebars,
           HTML, CSS, Bootstrap, Materialize, APIs, Heroku, Terminal, AJAX, Restful API, React Hooks, JSON, Sequelize, Java, Springboot,
           debugging, Excel, Vue.js, Python.</p>
-        </Row>
-        <Row className='skillsBar'>
+        </div>
+        <div className='skillsBar'>
             <h6>Skill Level</h6>
             <SkillsBar name="HTML5" value={90} />
             <SkillsBar name="CSS3" value={90} />
@@ -46,14 +40,24 @@ function Resume() {
             <SkillsBar name="Java" value={85} />
             <SkillsBar name="Node.js" value={85} />
             <SkillsBar name="Python" value={70} />
-        </Row>
-        <Row>
+          </div>
+          </Col>
+          <Col size="md-8 sm-6 justify-content-md-center">
+          <div>
+          <h2>PROFESSIONAL SUMMARY</h2>
+          <p>Front-End React Developer/Full Stack Developer leveraging nine years in oil and gas. Has a strong passion for innovation and a
+          keen eye for aesthetics and presentation as well as knowledge of Continuous integration/deployment. Recently obtained a certificate
+          in web development from the University of Texas, gaining experience while also honing my skills in HTML, CSS , Bootstrap, Javascript,
+          React and SQL. Excellent communication skills and collaborator who works well on team-based projects as well as independently. Recognized
+          for problem-solving and ability to adapt and implement new information as a part of a fast-paced environment.</p>
+        </div>
+        <div>
           <h2>PROFESSIONAL EXPERIENCE</h2>
-        </Row>
-        <Row>
+        </div>
+        <div>
           <h4>Spatial Consulting</h4>
-        </Row>
-        <Row>
+        </div>
+        <div>
           <h5>Full Stack Developer - Houston, TX | 01/2018-Present</h5>
           <p>Interfacing and translating client’s business requirements into technical applications and solutions. Tasked to handle end-to-end 
           software development in a timely manner to meet contract deadlines while also assisting more experienced developers re-factor and 
@@ -69,11 +73,11 @@ function Resume() {
             <li>Collaborate with team members to create mobile friendly UI.</li>
             <li>Constant learning through online resources to implement the latest software trends and libraries (i.e React.js, Angular.js, Vue.js</li>
           </ul>
-        </Row>
-        <Row>
+        </div>
+        <div>
           <h4>Wood</h4>
-        </Row>
-        <Row>
+        </div>
+        <div>
           <h4>Multi-Skilled Operator (MSO) - South, TX | 10/2011 - 12/2018</h4>
           <p>Keep track of all single-well and multi-well facilities oil and natural gas production. Apply proper maintenance to equipment when
             and if necessary.</p>
@@ -82,21 +86,21 @@ function Resume() {
             <li>- Assigned to largest production facilities owned by company</li>
             <li>- Awarded safety leader in 2012</li>
           </ul>
-        </Row>
-        <Row>
+        </div>
+        <div>
           <h2>Education</h2>
-        </Row>
-        <Row>
+        </div>
+        <div>
           <h4>Full Stack Web Development Certificate - University of Texas​; Houston, TX | 2020 </h4>
-        </Row>
-        <Row>
+        </div>
+        <div>
           <p>A 24-week intensive boot camp program focused on gaining technical programming and market-driven skills.</p>
-          <Col size="md-12 sm-6 justify-content-md-center">
+          <div size="md-12 sm-6 justify-content-md-center">
             <img className="bootcampCert" height="400px" src="./assets/bootcampCert.png" alt={"Error"} />
+          </div>
+        </div>
           </Col>
-        </Row>
-
-        
+          </Row>
       </Container>
         
       </div>
